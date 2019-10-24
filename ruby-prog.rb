51ee3c92235cfcf17e68c46fd1358f02b0531372
Louise <louise_flanagan@outlook.com>
2019-10-25 09:21:56 +1100

    added list of favourites

---
+++ ruby-prog.rb
@@ -0,0 +1,9 @@
+#Ruby program challenge to print favourites
+
+#Louise's favourites
+fav_food = "cottage pie "
+fav_drink = "negroni "
+fav_colour = "yellow "
+
+puts "Hey Lou, I know your favourite things... #{fav_food}, #{fav_drink} and the colour #{fav_colour}."
+puts "Cottage pie and Negroni. Ew."
