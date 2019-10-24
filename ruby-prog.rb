87caae1fb3a2b54da9bd7e956ef3bd9e9a3c5a44
Louise <louise_flanagan@outlook.com>
2019-10-25 09:33:50 +1100

    changed formatting

--- ruby-prog.rb
+++ ruby-prog.rb
@@ -5,15 +5,18 @@ fav_food = "cottage pie "
 fav_drink = "negroni "
 fav_colour = "yellow "
 
-puts "Hey Lou, I know your favourite things... #{fav_food}, #{fav_drink} and the colour #{fav_colour}."
-puts "Cottage pie and Negroni. Ew."
-print "Do you know what the difference between cottage pie and shepherds pie is?"
+puts "Hey Lou, I know your favourite things..." 
+puts "#{fav_food}, #{fav_drink} and the colour #{fav_colour}."
+puts "#{fav_food} and #{fav_drink}. Ew. Gross."
+puts "Do you know what the difference between cottage pie and shepherds pie is?"
 
 yes_no = gets.chomp.downcase
 
 if yes_no == "yes"
     puts "oh fine then. Mr smarty pants."
 elsif yes_no == "no"
-    puts "cottage pie is made with moo moos and shepherds pie is made with baa baas. duh!"
+    puts "#{fav_food} is made with moo moos and shepherds pie is made with baa baas. duh!"
 end
+
+
 
