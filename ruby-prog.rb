a411419ee67704646c0cc94f00a12538feee85b1
Louise <louise_flanagan@outlook.com>
2019-10-25 09:29:03 +1100

    what is shepherds or cottage pie made from

--- ruby-prog.rb
+++ ruby-prog.rb
@@ -7,3 +7,13 @@ fav_colour = "yellow "
 
 puts "Hey Lou, I know your favourite things... #{fav_food}, #{fav_drink} and the colour #{fav_colour}."
 puts "Cottage pie and Negroni. Ew."
+print "Do you know what the difference between cottage pie and shepherds pie is?"
+
+yes_no = gets.chomp.downcase
+
+if yes_no == "yes"
+    puts "oh fine then. Mr smarty pants."
+elsif yes_no == "no"
+    puts "cottage pie is made with moo moos and shepherds pie is made with baa baas. duh!"
+end
+
